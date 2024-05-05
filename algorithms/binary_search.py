3a2aef5cb364d6c1611d3aa77f02dc4f743d5b9b
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-05 16:18:05 -0300

    first algorithm

---
+++ algorithms/binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(a_list, item):
+    low = 0
+    high = len(a_list) - 1 # binary search, starts with 0, that's why we have -1. 
+
+    while low <= high:
+        midle = (low + high) // 2 
+        bet = a_list[midle]  
+        
+        if bet == item:  
+           return midle
+        elif bet > item: 
+            high = midle - 1
+        else:
+            low = midle + 1  
+    return None
+
+num_list = [1, 3, 4, 5, 6]
+
+print(binary_search(num_list, 6))
+
+        
