b018cef589f7e9c9261bdf0b6f755911a3116d40
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-07 22:03:56 -0300

    two_sum_algorithm

---
+++ two_sum.py
@@ -0,0 +1,12 @@
+def twoSum(nums, target):
+    # first solution - brute force
+    n = len(nums)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+nums = [11, 2, 5, 7]
+target = 9
+twoSum(nums, target)
