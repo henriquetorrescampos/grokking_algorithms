c014d21bd68d2b8f19a1d46650aaa1a809d62cd8
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-23 14:50:53 -0300

    .

---
+++ recursive.py
@@ -0,0 +1,32 @@
+"""
+-- Recursive function --
+When writing a recursive function that involvs an array, base-case will be many times 
+either an empty array or am array with one element"""
+
+# def fat(x):
+#     if x == 1:
+#         return 1
+#     else:
+#         return x * fat(x - 1)
+    
+# print(fat(5))
+
+# def sum(nums):
+"""Recursive function to sum all the values inside the list"""
+#     if nums == []:
+#         return 0
+#     else:
+#         return nums[0] + sum(nums[1:])
+
+# print(sum([1, 2, 3]))
+
+def count(nums):
+    if nums == []:
+        return 0
+    else:
+        return 1 + count(nums[1:])
+
+print(count([1, 2, 3, 9, 9, 9, 0]))
+
+
+
