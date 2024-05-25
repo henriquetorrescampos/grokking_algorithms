b7aa5b37989e92cd919a1f6c4d90590df2bf17e9
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-24 23:21:53 -0300

    two sum o(n)

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,29 @@
-def twoSum(nums, target):
-    # first solution - brute force
-    n = len(nums)
-    for i in range(n - 1):
-        for j in range(i + 1, n):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
+# def twoSum(nums, target):
+#     # first solution - brute force, O(n**2)
+#     n = len(nums)
+#     for i in range(n - 1):
+#         for j in range(i + 1, n):
+#             if nums[i] + nums[j] == target:
+#                 return [i, j]
+#     return []
 
-nums = [11, 2, 5, 7]
-target = 9
-twoSum(nums, target)
+# nums = [11, 2, 5, 7]
+# target = 9
+# twoSum(nums, target)
+
+def two_sum(nums, target):
+    dict = {}
+    for index, value in enumerate(nums):
+        difference = target - value # 5 - 2 = 3, 5 - 3 = 2
+        if difference in dict: # se diferente estiver em dict 
+            # print([dict[difference]]) # chamo o [dicionario[key]] pego valor {4: 2}, pego o valor 2
+            return [dict[difference], index]
+        else:
+            dict[value] = index # dict[2] = 0, {2: 0, }
+
+
+print(two_sum([2, 3, 4, 4], 8))
+
+
 
 
