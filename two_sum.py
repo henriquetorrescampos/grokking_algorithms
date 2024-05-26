c10c705f2c33663e9df79d5af2e132135c8c1236
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-25 22:43:34 -0300

    another way two sum

--- two_sum.py
+++ two_sum.py
@@ -11,19 +11,29 @@
 # target = 9
 # twoSum(nums, target)
 
+# def two_sum(nums, target):
+#     dict = {}
+#     for index, value in enumerate(nums):
+#         difference = target - value # 5 - 2 = 3, 5 - 3 = 2
+#         if difference in dict: # se diferente estiver em dict 
+#             # print([dict[difference]]) # chamo o [dicionario[key]] pego valor {4: 2}, pego o valor 2
+#             return [dict[difference], index]
+#         else:
+#             dict[value] = index # dict[2] = 0, {2: 0, }
+
+
+# print(two_sum([2, 3, 4, 4], 8))
+
+
 def two_sum(nums, target):
     dict = {}
     for index, value in enumerate(nums):
-        difference = target - value # 5 - 2 = 3, 5 - 3 = 2
-        if difference in dict: # se diferente estiver em dict 
-            # print([dict[difference]]) # chamo o [dicionario[key]] pego valor {4: 2}, pego o valor 2
-            return [dict[difference], index]
-        else:
-            dict[value] = index # dict[2] = 0, {2: 0, }
+        dict[value] = index
+    
+    for index, value in enumerate(nums):
+        desired = target - value
+        if desired in dict and dict[desired] != index:
+            return index, dict[desired]
 
 
 print(two_sum([2, 3, 4, 4], 8))
-
-
-
-
