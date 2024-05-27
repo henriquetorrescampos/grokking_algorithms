b42df11c368d7ea33642c97a144d6e067f94331f
Henrique Torres <henriquetorrescampos@gmail.com>
2024-05-26 23:47:11 -0300

    .

--- factorial.py
+++ factorial.py
@@ -1,10 +1,13 @@
-def factorial(n):
-    result = 1
-    while n > 1:
-        result = n * result
-        n -= 1
-    print(result)
+# def factorial(n):
+#     result = 1
+#     while n > 1:
+#         result = n * result
+#         n -= 1
+#     print(result)
 
-n = 5
+# n = 5
 
-factorial(n)
+# factorial(n)
+import math
+
+print(math.log2(100))
--- two_sum.py
+++ two_sum.py
@@ -11,15 +11,16 @@
 # target = 9
 # twoSum(nums, target)
 
-# def two_sum(nums, target):
-#     dict = {}
-#     for index, value in enumerate(nums):
-#         difference = target - value # 5 - 2 = 3, 5 - 3 = 2
-#         if difference in dict: # se diferente estiver em dict 
-#             # print([dict[difference]]) # chamo o [dicionario[key]] pego valor {4: 2}, pego o valor 2
-#             return [dict[difference], index]
-#         else:
-#             dict[value] = index # dict[2] = 0, {2: 0, }
+def two_sum(nums, target):
+    dict = {}
+    for index, value in enumerate(nums):
+        difference = target - value # 5 - 2 = 3, 5 - 3 = 2
+        print(dict)
+        if difference in dict: # se diferente estiver em dict 
+            # print([dict[difference]]) # chamo o [dicionario[key]] pego valor {4: 2}, pego o valor 2
+            return [dict[difference], index]
+        else:
+            dict[value] = index # dict[2] = 0, {2: 0} / dict[3] = 1, {3: 1} / dict[4] = 2 {4: 2}
 
 
-# print(two_sum([2, 3, 4, 4], 8))
+print(two_sum([2, 3, 4, 4], 8))
