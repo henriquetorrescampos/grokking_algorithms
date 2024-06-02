f0a1dc36dcbff3c84d1965a560eee78ae196943b
Henrique Torres <henriquetorrescampos@gmail.com>
2024-06-02 19:54:30 -0300

    .

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,8 @@
 def insertion_sort(my_list):
     for i in range(1, len(my_list)):
-        number_to_order = my_list[i]
+        number_to_order = my_list[i] 
         j = i - 1
-        while j >= 0 and number_to_order < my_list[j]:
+        while j >= 0 and number_to_order < my_list[j]: 
             my_list[j+1] = my_list[j]
             j -= 1
         my_list[j+1] = number_to_order
