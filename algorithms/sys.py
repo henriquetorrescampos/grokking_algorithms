15c0c8c31057d8313b4620965444f83399dda460
Henrique Torres <henriquetorrescampos@gmail.com>
2024-06-16 20:33:40 -0300

    recursive

--- algorithms/sys.py
+++ algorithms/sys.py
@@ -1,5 +1,22 @@
-string = 'testando'
-i = 0
-while i < len(string):
-    print(string[i])
-    i += 1
+# string = 'testando'
+# i = 0
+# while i < len(string):
+#     print(string[i])
+#     i += 1
+
+def permutation(array, n):
+    if n == 1:
+        print(array)
+        return 
+    
+    for i in range(n):
+        permutation(array, n - 1)
+        if n % 2 == 0:
+            array[i], array[n-1] = array[n-1], array[i]
+        else:
+            array[0], array[n-1] = array[n-1], array[0]
+    return array
+
+array = [1, 2, 3]
+
+permutation(array, len(array))
