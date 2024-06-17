024aea38550ee6a3f62dab2c7beebbe762af74e6
Henrique Torres <henriquetorrescampos@gmail.com>
2024-06-16 22:01:41 -0300

    fibonacci

--- fibonacci2.py
+++ fibonacci2.py
@@ -1,6 +1,14 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n - 1) + fibonacci(n - 2)
+# def fibonacci(n):
+#     if n <= 1:
+#         return n
+#     return fibonacci(n - 1) + fibonacci(n - 2)
 
-print(fibonacci(6))
+# print(fibonacci(6))
+
+def fa(n):
+    r = 1
+    for i in range(1, n+1):
+        r *= i
+    return r
+
+print(fa(5))
