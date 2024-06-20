08aebffb85cf8d6b254ac4acd88a4b32c3f5b466
Henrique Torres <henriquetorrescampos@gmail.com>
2024-06-19 21:59:50 -0300

    buble

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,4 +1,12 @@
-let array = [1, 2, 3, 4]
+let array = [5, 3, 4, 1]
 
-console.log(array)
+for (let i=0; i<array.length; ++i) {
+    for (let j=0; j<array.length - 1 - i; ++j) {
+        if (array[j] > array[j + 1]){
+            const temp = array[j]; 
+            array[j] = array[j + 1] ;
+            array[j + 1] = temp; 
+        }
+    }
+}
 
