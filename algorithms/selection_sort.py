b8ca3a212295c8433e9e1e9faa9b160433d30946
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-06-28 17:36:04 -0300

    selection_sort

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -1,13 +1,25 @@
 def selection_sort(my_list):
-    list_len = len(my_list)
-    for i in range(list_len - 1):
-        lowest = my_list[i] # the value
-        index = i # the position
-        for j in range(i + 1, list_len): #complexity of O(n**2)
-            if my_list[j] < lowest:
-                index = j
-                lowest = my_list[j]
-        my_list[i], my_list[index] = my_list[index], my_list[i]
-        return lowest, my_list
+    lowest = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] < lowest:
+            lowest = my_list[i]
+    return lowest
     
 print(selection_sort([9, 1, 5, 20, 10]))
+
+
+def select_sort(array):
+    array_len = len(array)
+
+    for i in range(array_len - 1):
+        lower_index = i
+        for j in range(i + 1, array_len):
+            if array[j] < array[lower_index]:
+                lower_index = j
+        
+        array[i], array[lower_index] = array[lower_index], array[i]
+    
+    return array
+
+array = [10, 20, 1, 2]
+print(f'Sorted array: {select_sort(array)}')
