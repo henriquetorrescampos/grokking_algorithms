d0ff6d6312c87ad681a39e84c69917fab3ae443e
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-03 19:44:42 -0300

    add new algorithm reversing an array

---
+++ algorithms/reverse_list.py
@@ -0,0 +1,16 @@
+def reverse(array):
+    len_array = len(array)
+    for i in range(len_array // 2):
+        array[i], array[len_array -1 - i] = array[len_array - 1 - i], array[i]
+    return array
+
+array = [1, 2, 3, 4]
+
+reverse(array)
+
+''' 
+Se iterarmos por toda comprimento da lista vamos acabar retornando a lista original.
+Quando dividimos por 2 asseguramos que o loop irá iterar até a metade da lista
+Ao trocarmos os elementos das extremidads indo ao centro garantimos a passagem em toda lista
+'''
+
