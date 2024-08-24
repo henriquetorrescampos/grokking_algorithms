4227a3911144549d450aeea4894a68155f3b029e
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-24 15:49:45 -0300

    n -1

--- algorithms/remove_element.py
+++ algorithms/remove_element.py
@@ -4,7 +4,7 @@ def remove_element(nums, val):
         if nums[i] != val: # se nums[0] diferente de 5, 3 != 5
             nums[k] = nums[i]
             k += 1 #contador, conta o meu k que vai ser o meu output
-    nums[:] = nums[:k] # [:] do inicio ate o final da array recebe a nova array de k de 0 ate o k
+    nums[:] = nums[:k] # [:] do inicio ate o final da array recebe a nova array de k de 0 ate o k - 1
     return nums, k
 
 n = 5
@@ -13,12 +13,3 @@ nums = [3, 5, 4, 1, 5, 2]
 print(remove_element(nums, n))
 
 
-# [3, 4, 1, 2]
-"""
-3 != 5 sim, nums[0] = nums[0], k=1
-5 != 5 nao,
-4 != 5, sim, nums[1] = nums[2], k=2
-1 != 5, sim, nums[2] = nums[3], k=3
-5 != 5, nao
-2 != 5, sim, nums[3] = nums[5], k=4
-"""
