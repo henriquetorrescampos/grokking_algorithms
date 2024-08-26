8506fd0d02a60cd0dab37aec006872ee2b61e82d
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-08-26 12:10:03 -0300

    .

--- algorithms/README.md
+++
@@ -1,5 +0,0 @@
-
-# Grokking Algorithms
-
-This repository is for my continuing studies in algorithms with companion book Grokking Algorithms.
-
--- algorithms/remove_duplicate.py
+++ algorithms/remove_duplicate.py
@@ -1,4 +1,4 @@
-nums = [1, 2, 3, 3]
+nums = [1, 1, 3, 3]
 nums2 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 target = 2
 
@@ -13,12 +13,15 @@ def remove_duplicate(nums):
 print(remove_duplicate(nums))
 print(remove_duplicate(nums2))
 
+s = 'abbaca'
+string_to_list = []
 
-'''
-nums[1] != nums[0]
-nums[1] = 
+for i in range(len(s)):
+    if len(string_to_list) > 0 and string_to_list[-1] == s[i]:
+        string_to_list.pop()
+    else:
+        string_to_list.append(s[i])
 
+output = ''.join(string_to_list)
+print(output)
 
-
-
-'''
