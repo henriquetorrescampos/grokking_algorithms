6da50b8c230362b2ab7d10513bc574d60604cc9c
henriquetorrescampos <henriquetorrescampos@gmail.com>
2024-09-02 15:38:24 -0300

    .

--- searchSorted/selection_sort.py
+++ searchSorted/selection_sort.py
@@ -23,3 +23,19 @@ def lowest_num(my_list):
     
 print(lowest_num([9, 1, 5, 20, 10]))
 
+
+#              0           1         2          3           4           5
+intervals = [(8, 9), (11, 11.25), (9, 11), (11.5, 13), (10, 10.75), (8, 9)]
+#             0, 1     0 , 1       0, 1      0,   1      0,   1      0,  1
+intervals.sort(key=lambda x: x[0])
+
+count = 1
+end = intervals[0][1]
+
+for interval in intervals[1:]:
+    if interval[0] > end:
+        end = interval[1]
+        print(end)
+        count += 1
+
+print(count)
